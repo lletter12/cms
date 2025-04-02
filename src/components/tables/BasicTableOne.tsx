230a5b92ccbccd4024033b996a16d754d2568025
lletter81 <lletter81@naver.com>
2025-04-02 16:47:25 +0900

    14th commit

--- src/components/tables/BasicTableOne.tsx
+++ src/components/tables/BasicTableOne.tsx
@@ -30,16 +30,16 @@ const tableData: Order[] = [
     {
         id: 1,
         user: {
-            image: "/images/user/user-17.jpg",
-            name: "Lindsey Curtis",
+            image: "/logo.png",
+            name: "Admin",
             role: "Web Designer",
         },
         projectName: "Agency Website",
         team: {
             images: [
-                "/images/user/user-22.jpg",
-                "/images/user/user-23.jpg",
-                "/images/user/user-24.jpg",
+                "/logo.png",
+                "/logo.png",
+                "/logo.png",
             ],
         },
         budget: "3.9K",
@@ -48,13 +48,13 @@ const tableData: Order[] = [
     {
         id: 2,
         user: {
-            image: "/images/user/user-18.jpg",
-            name: "Kaiya George",
+            image: "/logo.png",
+            name: "Admin",
             role: "Project Manager",
         },
         projectName: "Technology",
         team: {
-            images: ["/images/user/user-25.jpg", "/images/user/user-26.jpg"],
+            images: ["/logo.png", "/logo.png"],
         },
         budget: "24.9K",
         status: "Pending",
@@ -62,13 +62,13 @@ const tableData: Order[] = [
     {
         id: 3,
         user: {
-            image: "/images/user/user-17.jpg",
-            name: "Zain Geidt",
+            image: "/logo.png",
+            name: "Admin",
             role: "Content Writing",
         },
         projectName: "Blog Writing",
         team: {
-            images: ["/images/user/user-27.jpg"],
+            images: ["/logo.png"],
         },
         budget: "12.7K",
         status: "Active",
@@ -76,16 +76,16 @@ const tableData: Order[] = [
     {
         id: 4,
         user: {
-            image: "/images/user/user-20.jpg",
-            name: "Abram Schleifer",
+            image: "/logo.png",
+            name: "Admin",
             role: "Digital Marketer",
         },
         projectName: "Social Media",
         team: {
             images: [
-                "/images/user/user-28.jpg",
-                "/images/user/user-29.jpg",
-                "/images/user/user-30.jpg",
+                "/logo.png",
+                "/logo.png",
+                "/logo.png",
             ],
         },
         budget: "2.8K",
@@ -94,16 +94,16 @@ const tableData: Order[] = [
     {
         id: 5,
         user: {
-            image: "/images/user/user-21.jpg",
-            name: "Carla George",
+            image: "/logo.png",
+            name: "Admin",
             role: "Front-end Developer",
         },
         projectName: "Website",
         team: {
             images: [
-                "/images/user/user-31.jpg",
-                "/images/user/user-32.jpg",
-                "/images/user/user-33.jpg",
+                "/logo.png",
+                "/logo.png",
+                "/logo.png",
             ],
         },
         budget: "4.5K",
@@ -169,12 +169,12 @@ export default function BasicTableOne() {
                                                 />
                                             </div>
                                             <div>
-                        <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                          {order.user.name}
-                        </span>
-                                                <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
-                          {order.user.role}
-                        </span>
+                                                <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
+                                                  {order.user.name}
+                                                </span>
+                                                                        <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
+                                                  {order.user.role}
+                                                </span>
                                             </div>
                                         </div>
                                     </TableCell>
