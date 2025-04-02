2658b162417af0ec65938bfa3179736ca385506a
lletter81 <lletter81@naver.com>
2025-04-02 16:42:10 +0900

    13th commit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,13 +3,13 @@ import {LocaleDropdown} from "@/components/localDropdown/LocalDropDown";
 
 export default function Home() {
     return (
-        <div className="min-h-screen xl:flex dark:bg-gray-900 font-[family-name:var(--font-geist-sans)] px-5">
+        <div className="min-h-screen xl:flex dark:bg-gray-900 font-[family-name:var(--font-geist-sans)]">
             <div className={"relative"}>
                 <div className={"absolute top-3 right-3.5"}>
                     <LocaleDropdown/>
                 </div>
             </div>
-            <div className="flex min-w-screen min-h-screen">
+            <div className="flex min-w-screen min-h-screen px-5">
                 <SignInForm/>
             </div>
         </div>
