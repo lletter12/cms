b6cfe23f28c4959bde325e0b9c195529f823b566
lletter81 <lletter81@naver.com>
2025-04-02 16:56:42 +0900

    15th commit

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -7,8 +7,10 @@ import Image from "next/image";
 import React, {useEffect, useRef, useState} from "react";
 import {LocaleDropdown} from "@/components/localDropdown/LocalDropDown";
 import {useRouter} from "@/i18n/navigation";
+import {useTranslations} from "next-intl";
 
 const AppHeader: React.FC = () => {
+    const t = useTranslations("Header");
     const [isApplicationMenuOpen, setApplicationMenuOpen] = useState(false);
     const {isMobileOpen, toggleSidebar, toggleMobileSidebar} = useSidebar();
     const router = useRouter();
@@ -142,7 +144,7 @@ const AppHeader: React.FC = () => {
                                 <input
                                     ref={inputRef}
                                     type="text"
-                                    placeholder="Search or type command..."
+                                    placeholder={t("text.search.placeHolder")}
                                     className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-200 bg-transparent py-2.5 pl-12 pr-14 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-800 dark:bg-gray-900 dark:bg-white/[0.03] dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 xl:w-[430px]"
                                 />
 
